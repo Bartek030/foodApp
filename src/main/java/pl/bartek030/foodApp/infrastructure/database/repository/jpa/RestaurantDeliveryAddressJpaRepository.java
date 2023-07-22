9022604fdfa042d182fd1c7c3797b9dc430665fa
Bartlomiej Swies <bartlomiejswies@gmail.com>
2023-07-22 21:31:40 +0200

    Imports optimization

--- src/main/java/pl/bartek030/foodApp/infrastructure/database/repository/jpa/RestaurantDeliveryAddressJpaRepository.java
+++ src/main/java/pl/bartek030/foodApp/infrastructure/database/repository/jpa/RestaurantDeliveryAddressJpaRepository.java
@@ -2,7 +2,6 @@ package pl.bartek030.foodApp.infrastructure.database.repository.jpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import pl.bartek030.foodApp.infrastructure.database.entity.AddressEntity;
 import pl.bartek030.foodApp.infrastructure.database.entity.RestaurantDeliveryAddressEntity;
 
 @Repository
