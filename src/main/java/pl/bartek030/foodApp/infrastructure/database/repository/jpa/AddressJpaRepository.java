10800065969c147ae69c9cd4828e3b19d963651e
Bartlomiej Swies <bartlomiejswies@gmail.com>
2023-07-22 17:56:00 +0200

    Jpa repository creation part 2

---
+++ src/main/java/pl/bartek030/foodApp/infrastructure/database/repository/jpa/AddressJpaRepository.java
@@ -0,0 +1,9 @@
+package pl.bartek030.foodApp.infrastructure.database.repository.jpa;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pl.bartek030.foodApp.infrastructure.database.entity.AddressEntity;
+
+@Repository
+public interface AddressJpaRepository extends JpaRepository<AddressEntity, Long> {
+}
