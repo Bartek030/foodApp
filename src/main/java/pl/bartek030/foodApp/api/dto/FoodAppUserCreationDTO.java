58248fe7c7081a03ae2a40987069bfbaace817ed
Bartlomiej Swies <bartlomiejswies@gmail.com>
2023-08-13 12:18:56 +0200

    Input data validation

--- src/main/java/pl/bartek030/foodApp/api/dto/FoodAppUserCreationDTO.java
+++ src/main/java/pl/bartek030/foodApp/api/dto/FoodAppUserCreationDTO.java
@@ -1,5 +1,7 @@
 package pl.bartek030.foodApp.api.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,8 +14,11 @@ import lombok.NoArgsConstructor;
 public class FoodAppUserCreationDTO {
     private String name;
     private String surname;
+    @Email
     private String email;
+    @Pattern(regexp = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$")
     private String password;
+    @Pattern(regexp = "/\\(?([0-9]{3})\\)?([ .-]?)([0-9]{3})\\2([0-9]{4})/")
     private String phone;
     private String country;
     private String city;
