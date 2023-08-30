69a2b7cab9b2270febf521dc693e175190c31779
Bartlomiej Swies <bartlomiejswies@gmail.com>
2023-08-30 19:20:03 +0200

    Wiremock test implementation

--- src/test/java/pl/bartek030/foodApp/backendTests/WeatherApiWireMockTestIT.java
+++ src/test/java/pl/bartek030/foodApp/backendTests/WeatherApiWireMockTestIT.java
@@ -10,22 +10,22 @@ import pl.bartek030.foodApp.configuration.support.WireMockTestSupport;
 public class WeatherApiWireMockTestIT extends RestAssuredIntegrationTestBase
         implements WireMockTestSupport, WeatherApiControllerTestSupport {
 
-//    @Test
-//    void shouldReturnWeatherDataCorrectly() {
-//        // given
-//        String cityName = "Warszawa";
-//        Double temperature = 304.25;
-//        Integer pressure = 1017;
-//        Double windSpeed = 1.54;
-//
-//        stubForWeatherApi(wireMockServer, cityName);
-//
-//        // when
-//        final WeatherDataDTO weatherData = getWeatherData(cityName);
-//
-//        // then
-//        Assertions.assertEquals(temperature, weatherData.getMain().getTemp());
-//        Assertions.assertEquals(pressure, weatherData.getMain().getPressure());
-//        Assertions.assertEquals(windSpeed, weatherData.getWind().getSpeed());
-//    }
+    @Test
+    void shouldReturnWeatherDataCorrectly() {
+        // given
+        String cityName = "Warszawa";
+        Double temperature = 304.25;
+        Integer pressure = 1017;
+        Double windSpeed = 1.54;
+
+        stubForWeatherApi(wireMockServer, cityName);
+
+        // when
+        final WeatherDataDTO weatherData = getWeatherData(cityName);
+
+        // then
+        Assertions.assertEquals(temperature, weatherData.getMain().getTemp());
+        Assertions.assertEquals(pressure, weatherData.getMain().getPressure());
+        Assertions.assertEquals(windSpeed, weatherData.getWind().getSpeed());
+    }
 }
